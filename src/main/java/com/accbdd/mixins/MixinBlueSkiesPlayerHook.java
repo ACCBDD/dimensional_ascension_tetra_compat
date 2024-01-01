b5be97f58f1d295671f0f0139d9c29cf952bf86d
ACCBDD <willinghamnottel@gmail.com>
2024-01-01 03:03:42 -0800

    make mixin function returnable (whoops!)

--- src/main/java/com/accbdd/mixins/MixinBlueSkiesPlayerHook.java
+++ src/main/java/com/accbdd/mixins/MixinBlueSkiesPlayerHook.java
@@ -5,7 +5,7 @@ import javax.annotation.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.legacy.blue_skies.asm_hooks.PlayerHooks;
 
@@ -16,7 +16,7 @@ import net.minecraft.world.level.block.state.BlockState;
 public class MixinBlueSkiesPlayerHook {
 
     @Inject(method = "isNerfableTool", at = @At("HEAD"), remap=false)
-    protected void checkIfBlueSkiesTetraTool(@Nullable ItemStack stack, @Nullable BlockState state, CallbackInfo ci) {
+    protected void checkIfBlueSkiesTetraTool(@Nullable ItemStack stack, @Nullable BlockState state, CallbackInfoReturnable<Boolean> ci) {
         System.out.print(stack.serializeNBT());
     }
 }
