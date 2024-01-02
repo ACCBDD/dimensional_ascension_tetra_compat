b7863a1c2cbc94c009a2c624c7efd29d5a20c3af
ACCBDD <willinghamnottel@gmail.com>
2024-01-02 02:29:29 -0800

    branched autosmelting garbage away (god)

--- src/main/java/com/accbdd/dimasctetracompat/DimensionalAscension.java
+++ src/main/java/com/accbdd/dimasctetracompat/DimensionalAscension.java
@@ -1,15 +1,7 @@
 package com.accbdd.dimasctetracompat;
 
-import com.accbdd.dimasctetracompat.loot.HasBlockEntity;
-import com.accbdd.dimasctetracompat.loot.HasScorching;
-import com.accbdd.dimasctetracompat.loot.modifier.ScorchingLootModifier;
-import com.mojang.serialization.Codec;
-
-import net.minecraft.core.Registry;
-import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,9 +11,6 @@ import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(DimensionalAscension.MODID)
@@ -29,15 +18,6 @@ public class DimensionalAscension
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "dimasctetracompat";
-    
-    //loot modifier for autosmelt
-    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIERS = DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, MODID);
-    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> SCORCHING_MODIFIER = LOOT_MODIFIERS.register("scorching", () -> ScorchingLootModifier.CODEC);
-
-    //loot condition for safer autosmelt (don't smelt block entities)
-    public static final DeferredRegister<LootItemConditionType> LOOT_CONDITION_TYPES = DeferredRegister.create(Registry.LOOT_CONDITION_TYPE.key(), MODID);
-    public static final RegistryObject<LootItemConditionType> HAS_BLOCK_ENTITY = LOOT_CONDITION_TYPES.register("has_block_entity", () -> new LootItemConditionType(new HasBlockEntity.Serializer()));
-    public static final RegistryObject<LootItemConditionType> HAS_SCORCHING = LOOT_CONDITION_TYPES.register("has_scorching", () -> new LootItemConditionType(new HasScorching.Serializer()));
 
     public DimensionalAscension()
     {
@@ -51,12 +31,6 @@ public class DimensionalAscension
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        //register loot modifier
-        LOOT_MODIFIERS.register(modEventBus);
-
-        //register loot conditions
-        LOOT_CONDITION_TYPES.register(modEventBus);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
--- src/main/java/com/accbdd/dimasctetracompat/effects/ScorchingEffect.java
+++
@@ -1,9 +0,0 @@
-package com.accbdd.dimasctetracompat.effects;
-
-import se.mickelus.tetra.effect.ItemEffect;
-
-public class ScorchingEffect {
-    public ItemEffect getEffect() {
-        return ItemEffect.get("dimasctetracompat:scorching");
-    }
-}
